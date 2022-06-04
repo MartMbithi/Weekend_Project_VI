c3944c05921eea2fe0357dd64711d1da3f181cbb
Martin Mbithi <martdevelopers254@gmail.com>
2022-06-04 13:30:20 +0300

    Craft Customer Sign In Modal

--- views/login.php
+++ views/login.php
@@ -26,7 +26,7 @@ require_once('../partials/head.php');
 
                 <form method="post">
                     <div class="input-group mb-3">
-                        <input type="email" name="login_name" class="form-control" placeholder="Login Name / Email">
+                        <input type="email" required name="login_name" class="form-control" placeholder="Login Name / Email">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user-tag"></span>
@@ -34,7 +34,7 @@ require_once('../partials/head.php');
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" name="login_password" class="form-control" placeholder="Login Password">
+                        <input type="password" required name="login_password" class="form-control" placeholder="Login Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
@@ -55,10 +55,10 @@ require_once('../partials/head.php');
                 <hr>
                 <p class="mb-2">
                 <div class="social-auth-links text-center mb-3">
-                    <a href="sign_up?rank=Farmer" class="btn btn-block btn-primary">
+                    <a data-toggle="modal" href="#farmer_signup" class="btn btn-block btn-primary">
                         Sign Up As Farmer
                     </a>
-                    <a href="sign_up?rank=Customer" class="btn btn-block btn-danger">
+                    <a data-toggle="modal" href="#customer_signup" class="btn btn-block btn-danger">
                         Sign Up As Customer
                     </a>
                 </div>
@@ -67,6 +67,97 @@ require_once('../partials/head.php');
         <!-- /.login-card-body -->
     </div>
     <!-- /.login-box -->
+
+    <!-- Sign Up Modals -->
+    <div class="modal fade" id="farmer_signup">
+        <div class="modal-dialog modal-dialog-centered  modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Register New Farmer Account</h4>
+                    <button type="button" class="close" data-dismiss="modal">
+                        <span>&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" enctype="multipart/form-data">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label>Full Name</label>
+                                <input type="text" name="farmer_name" required class="form-control">
+                                <input type="hidden" value="Farmer" name="login_rank" required class="form-control">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Email</label>
+                                <input type="email" name="farmer_email" required class="form-control">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Phone Number</label>
+                                <input type="text" name="farmer_phone" required class="form-control">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Login Password</label>
+                                <input type="password" name="login_password" required class="form-control">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label>Address</label>
+                                <textarea type="text" name="product_description" rows="2" class="form-control"></textarea>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="text-right">
+                            <button name="register" class="btn btn-primary" type="submit">
+                                Register Farmer Account
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Customer Modal -->
+    <div class="modal fade" id="customer_signup">
+        <div class="modal-dialog modal-dialog-centered  modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Register New Customer Account</h4>
+                    <button type="button" class="close" data-dismiss="modal">
+                        <span>&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" enctype="multipart/form-data">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label>Full Name</label>
+                                <input type="text" name="customer_name" required class="form-control">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Email</label>
+                                <input type="email" name="customer_email" required class="form-control">
+                                <input type="hidden" value="Customer" name="login_rank" required class="form-control">
+
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Phone Number</label>
+                                <input type="text" name="customer_phone" required class="form-control">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Login Password</label>
+                                <input type="password" name="login_password" required class="form-control">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="text-right">
+                            <button name="register" class="btn btn-primary" type="submit">
+                                Register Customer Account
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Sign Up Modals -->
 
     <?php require_once('../partials/scripts.php'); ?>
 </body>
