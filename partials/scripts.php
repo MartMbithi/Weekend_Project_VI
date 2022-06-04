87e9430f15688a1f31f9c7fbcf0da06620e6a0d3
Martin Mbithi <martdevelopers254@gmail.com>
2022-06-04 14:01:53 +0300

    Prevent Form Resubmissions

--- partials/scripts.php
+++ partials/scripts.php
@@ -8,29 +8,59 @@
  <script src="../public/plugins/sweetalert2/sweetalert2.min.js"></script>
  <!-- Toastr -->
  <script src="../public/plugins/toastr/toastr.min.js"></script>
+ <script>
+     /* Stop Double Resubmission */
+     if (window.history.replaceState) {
+         window.history.replaceState(null, null, window.location.href);
+     }
+ </script>
+ <!-- Init Sweet Alerts -->
  <?php if (isset($success)) { ?>
      <!-- Pop Success Alert -->
      <script>
+         const Toast = Swal.mixin({
+             toast: true,
+             position: 'top-end',
+             showConfirmButton: false,
+             timer: 3000
+         });
          Toast.fire({
              type: 'success',
-             title: '<?php echo $success; ?>'
+             title: '<?php echo $success; ?>',
          })
      </script>
 
  <?php }
     if (isset($err)) { ?>
      <script>
+         /* Pop Error Message */
+         const Toast = Swal.mixin({
+             toast: true,
+             position: 'top-end',
+             showConfirmButton: false,
+             timer: 3000
+         });
          Toast.fire({
              type: 'error',
-             title: '<?php echo $err; ?>'
+             title: '<?php echo $err; ?>',
          })
      </script>
+
  <?php }
     if (isset($info)) { ?>
      <script>
+         /* Pop Warning  */
+         const Toast = Swal.mixin({
+             toast: true,
+             position: 'top-end',
+             showConfirmButton: false,
+             timer: 3000
+         });
          Toast.fire({
-             type: 'warning',
-             title: '<?php echo $info; ?>'
+             type: 'info',
+             title: '<?php echo $info; ?>',
          })
      </script>
- <?php } ?>
+
+ <?php }
+    ?>
