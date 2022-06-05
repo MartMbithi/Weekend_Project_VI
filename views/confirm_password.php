b58ad237feabf170a4a28fac62d90c0913a3e4a5
Martin Mbithi <martdevelopers254@gmail.com>
2022-06-05 10:22:33 +0300

    Implement Passwiord Reset UI

--- views/confirm_password.php
+++ views/confirm_password.php
@@ -48,9 +48,9 @@ require_once('../partials/head.php');
             <div class="card-body login-card-body">
                 <p class="login-box-msg">You are only one step a way from your new password, recover your password now.</p>
 
-                <form action="https://adminlte.io/themes/v3/pages/examples/login.html" method="post">
+                <form method="post">
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
+                        <input type="password" required name="new_password" class="form-control" placeholder="Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
@@ -58,7 +58,7 @@ require_once('../partials/head.php');
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Confirm Password">
+                        <input type="password" required name="confirm_password" class="form-control" placeholder="Confirm Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
@@ -67,15 +67,11 @@ require_once('../partials/head.php');
                     </div>
                     <div class="row">
                         <div class="col-12">
-                            <button type="submit" class="btn btn-primary btn-block">Change password</button>
+                            <button type="submit" name="confirm_password" class="btn btn-primary btn-block">Change password</button>
                         </div>
                         <!-- /.col -->
                     </div>
                 </form>
-
-                <p class="mt-3 mb-1">
-                    <a href="login.html">Login</a>
-                </p>
             </div>
             <!-- /.login-card-body -->
         </div>
