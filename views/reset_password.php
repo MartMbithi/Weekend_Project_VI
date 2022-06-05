9526cfdc6b16f2a480a4d7f39d1272f648598d9c
Martin Mbithi <martdevelopers254@gmail.com>
2022-06-05 10:07:12 +0300

    Inject Backend Logic On Password Reset

--- views/reset_password.php
+++ views/reset_password.php
@@ -11,7 +11,24 @@
  * can only be MART DEVELOPERS INC.
  *
  */
-require_once('../partials/head.php');g
+session_start();
+require_once('../config/config.php');
+if (isset($_POST['reset_password'])) {
+    $loin_email = mysqli_real_escape_string($mysqli, $_POST['login_email']);
+    /* Check If User Exists */
+    $sql = "SELECT * FROM  login WHERE login_email = '{$login_email}'";
+    $res = mysqli_query($mysqli, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        /* Redirect User To Confirm Password */
+        $_SESSION['success'] = 'Password Reset Token Generated, Proceed To Confirm Password';
+        $_SESSION['login_email'] = $user_email;
+        header('Location: confirm_password');
+        exit;
+    } else {
+        $err = "Email Address  Does Not Exist";
+    }
+}
+require_once('../partials/head.php');
 ?>
 
 <body class="hold-transition login-page">
@@ -26,7 +43,7 @@ require_once('../partials/head.php');g
 
                 <form method="post">
                     <div class="input-group mb-3">
-                        <input type="email" name="login_email" class="form-control" placeholder="Login Email">
+                        <input type="email" required name="login_email" class="form-control" placeholder="Login Email">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
