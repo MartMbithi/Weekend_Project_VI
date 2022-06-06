de1507f3fc19db9b8713ffa06e828918c3554a55
Martin Mbithi <martdevelopers254@gmail.com>
2022-06-06 14:33:38 +0300

    All Unit Tests Pass On Create, Read, Update & Delete Farmers

--- views/admin_farmers.php
+++ views/admin_farmers.php
@@ -239,6 +239,31 @@ require_once('../partials/head.php');
                                                         </div>
                                                     </div>
                                                 </div>
+                                                <!-- Delete Modal -->
+                                                <div class="modal fade" id="delete_<?php echo $farmer->farmer_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="exampleModalLabel">CONFIRM DELETE</h5>
+                                                                <button type="button" class="close" data-dismiss="modal">
+                                                                    <span>&times;</span>
+                                                                </button>
+                                                            </div>
+                                                            <form method="POST">
+                                                                <div class="modal-body text-center ">
+                                                                    <h4 class="text-danger">
+                                                                        Delete <?php echo  $farmer->farmer_name; ?> Account?
+                                                                    </h4>
+                                                                    <br>
+                                                                    <!-- Hide This -->
+                                                                    <input type="hidden" name="farmer_login_id" value="<?php echo $farmer->farmer_login_id; ?>">
+                                                                    <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
+                                                                    <button type="submit" class="text-center btn btn-danger" name="delete_farmer">Delete</button>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             <?php
                                             }
                                             ?>
