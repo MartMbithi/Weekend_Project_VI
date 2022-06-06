923042197d4c1a9db25729bf026a3ced6c3f93d1
Martin Mbithi <martdevelopers254@gmail.com>
2022-06-06 12:15:12 +0300

    Craft Top Navigation Bar

---
+++ partials/footer.php
@@ -0,0 +1,8 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <strong>Copyright &copy; <?php echo date('Y'); ?> Online Farmers Market Platform. </strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1.0.0
+    </div>
+</footer>
