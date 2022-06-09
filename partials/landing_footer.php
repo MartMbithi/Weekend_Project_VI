2bd0e56d0cd434d0c86d473b29c0a46bd24b43e0
Martin Mbithi <martdevelopers254@gmail.com>
2022-06-09 12:46:26 +0300

    Craft Landing Page - Footer

---
+++ partials/landing_footer.php
@@ -0,0 +1,75 @@
+  <!-- start footer -->
+  <footer id="footer" class="footer footer--style-4">
+      <div class="container">
+          <div class="row">
+              <div class="col-12 col-sm-4 col-md-3 col-lg-2">
+                  <div class="footer__item">
+                      <a class="site-logo" href="index.html">
+                          <img class="img-fluid  lazy" src="img/blank.gif" data-src="img/site_logo.png" alt="demo" />
+                      </a>
+                  </div>
+              </div>
+
+              <div class="col-12 col-md-9 col-lg-6">
+                  <div class="footer__item">
+                      <nav id="footer__navigation" class="navigation">
+                          <div class="row">
+                              <div class="col-6 col-sm-6">
+                                  <h5 class="footer__item__title h6">Menu</h5>
+                                  <ul>
+                                      <li class="active"><a href="../">Home</a></li>
+                                      <li><a href="landing_categories">Categories</a></li>
+                                      <li><a href="landing_products">Products</a></li>
+                                  </ul>
+                              </div>
+
+                              <div class="col-6 col-sm-6">
+                                  <h5 class="footer__item__title h6">Accounts</h5>
+                                  <ul>
+                                      <li><a href="login">Farmer Sign In</a></li>
+                                      <li><a href="login">Customer Sign In</a></li>
+                                      <li><a href="reset_password">Reset Password</a></li>
+                                  </ul>
+                              </div>
+                          </div>
+                      </nav>
+                  </div>
+              </div>
+
+              <div class="col-12 col-md col-lg-4">
+                  <div class="footer__item">
+                      <h5 class="footer__item__title h6">Contacts</h5>
+
+                      <address>
+                          <p>
+                              523 Sylvan Ave, 5th Floor Mountain View, Nairobi, Kenya
+                          </p>
+
+                          <p>
+                              +254 727 200037
+                          </p>
+
+                          <p>
+                              <a href="mailto:support@onlinefarmers.com">support@onlinefarmersstore.com</a>
+                          </p>
+                      </address>
+
+                      <div class="social-btns">
+                          <a href="#"><i class="fontello-twitter"></i></a>
+                          <a href="#"><i class="fontello-facebook"></i></a>
+                          <a href="#"><i class="fontello-linkedin-squared"></i></a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div class="row align-items-lg-end justify-content-lg-between copyright">
+              <div class="col-12 col-lg-12">
+                  <div class="footer__item">
+                      <span class="__copy">© <?php echo date('Y'); ?>, Online Farmers Market Platform. All Rights Reserved</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </footer>
+  <!-- end footer -->
